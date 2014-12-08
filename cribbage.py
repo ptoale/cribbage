40183c30f14fd44bd8adef64e41f2bb12e63e316
ptoale <ptoale@gmail.com>
2014-12-07 20:40:56 -0600

    added histogram of results

--- cribbage.py
+++ cribbage.py
@@ -17,36 +17,37 @@ def score_hand(hand, starter, verbose=False):
     :param verbose: flag for printing
     :return: the score of the hand
 
+    >>> from card import Card
+    >>> hand = [Card(Rank.jack, Suit.hearts), \
+                Card(Rank.five, Suit.spades), \
+                Card(Rank.five, Suit.clubs),  \
+                Card(Rank.five, Suit.diamonds)]
+    >>> starter = Card(Rank.five, Suit.hearts)
+    >>> score_hand(hand, starter, False)
+    29
 
     """
-    pts = 0
-
     p_15 = score_15(hand, starter)
-    pts += p_15
     if verbose:
-        print "  15s:   {:>2}".format(p_15)
+        print("  15s:   {:>2}".format(p_15))
 
     p_pair = score_pairs(hand, starter)
-    pts += p_pair
     if verbose:
-        print "  pairs: {:>2}".format(p_pair)
+        print("  pairs: {:>2}".format(p_pair))
 
     p_run = score_runs(hand, starter)
-    pts += p_run
     if verbose:
-        print "  runs:  {:>2}".format(p_run)
+        print("  runs:  {:>2}".format(p_run))
 
     p_flush = score_flush(hand, starter)
-    pts += p_flush
     if verbose:
-        print "  flush: {:>2}".format(p_flush)
+        print("  flush: {:>2}".format(p_flush))
 
     p_nibs = score_nibs(hand, starter)
-    pts += p_nibs
     if verbose:
-        print "  nibs:  {:>2}".format(p_nibs)
+        print("  nibs:  {:>2}".format(p_nibs))
 
-    return pts
+    return p_15 + p_pair + p_run + p_flush + p_nibs
 
 
 def score_15(hand, starter):
@@ -75,13 +76,12 @@ def score_15(hand, starter):
     # iterate over all combinations of 2, 3, 4, and 5 cards
     for l in range(2,6):
         for cs in itertools.combinations(cards, l):
-            s = 0
             # sum up the values of this combination
+            s = 0
             for c in cs:
                 s += c.get_value()
             if s == 15:
                 # we have a 15
-#                print "   ", cs
                 count += 1
 
     # 2 points for each 15
@@ -117,7 +117,6 @@ def score_pairs(hand, starter):
     for cs in itertools.combinations(cards, 2):
         if cs[0].rank == cs[1].rank:
             # we have a pair
-#            print cs
             count += 1
 
     # 2 points for each pair
@@ -146,10 +145,10 @@ def score_runs(hand, starter):
                 0 1 1   0 1 1   0 1 4   0 2 0   0 2 3   0 4 1   0 1 0   0 1 3   0 1 3   0 0 3
 
     >>> from card import Card
-    >>> hand = [Card(Rank.jack, Suit.hearts), \
-                Card(Rank.ten,  Suit.spades), \
+    >>> hand = [Card(Rank.jack,  Suit.hearts), \
+                Card(Rank.ten,   Suit.spades), \
                 Card(Rank.nine,  Suit.clubs),  \
-                Card(Rank.queen,  Suit.diamonds)]
+                Card(Rank.queen, Suit.diamonds)]
     >>> starter = Card(Rank.king, Suit.hearts)
     >>> score_runs(hand, starter)
     5
@@ -174,7 +173,6 @@ def score_runs(hand, starter):
         d = c.del_rank(c0)
         ds.append(d)
         c0 = c
-#        print c, c0, d
 
     # if this matches, we have a run of 5 and can return
     if ds == [0, 1, 1, 1, 1]:
@@ -184,7 +182,6 @@ def score_runs(hand, starter):
     p = 0
     for cs in itertools.combinations(cards, 4):
         scards = sorted(cs)
-#        print scards
         ds = []
         c0 = scards[0]
 
@@ -192,7 +189,6 @@ def score_runs(hand, starter):
             d = c.del_rank(c0)
             ds.append(d)
             c0 = c
-#            print c, c0, d
 
         # if this matches, we have a run of 4
         if ds == [0, 1, 1, 1]:
@@ -205,7 +201,6 @@ def score_runs(hand, starter):
     p = 0
     for cs in itertools.combinations(cards, 3):
         scards = sorted(cs)
-#        print scards
         ds = []
         c0 = scards[0]
 
@@ -213,7 +208,6 @@ def score_runs(hand, starter):
             d = c.del_rank(c0)
             ds.append(d)
             c0 = c
-#            print c, c0, d
 
         # if this matches, we have a run of 3
         if ds == [0, 1, 1]:
@@ -238,7 +232,7 @@ def score_flush(hand, starter):
     >>> hand = [Card(Rank.jack, Suit.hearts), \
                 Card(Rank.ace,  Suit.hearts), \
                 Card(Rank.two,  Suit.hearts),  \
-                Card(Rank.four,  Suit.hearts)]
+                Card(Rank.four, Suit.hearts)]
     >>> starter = Card(Rank.five, Suit.hearts)
     >>> score_flush(hand, starter)
     5
@@ -296,9 +290,12 @@ if __name__ == '__main__':
 #   Play around
     verbose = False
     deck = Deck()
-    n_hands = 100000
+    n_hands = 1000000
+
+    hist = {}
 
     total = 0
+    mn = 30
     mx = -1
     for h in range(n_hands):
         deck.shuffle()
@@ -309,17 +306,26 @@ if __name__ == '__main__':
         starter = deck.deal.next()
 
         if verbose:
-            print "Hand {}: [{}] {} {} {} {}".format(h, starter, *hand)
+            print("Hand {}: [{}] {} {} {} {}".format(h, starter, *hand))
 
         pts = score_hand(hand, starter, verbose)
+        if pts in hist:
+            hist[pts] += 1
+        else:
+            hist[pts] = 1
 
         if verbose:
-            print "  Total: {:>2}".format(pts)
+            print("  Total: {:>2}".format(pts))
 
         if pts > mx:
             mx = pts
+        if pts < mn:
+            mn = pts
 
         total += pts
 
     avg = float(total)/n_hands
-    print "Avg = %f   Max = %d" % (avg, mx)
+    print("Avg = {:<6} Min = {:<2} Max = {:<2}".format(avg, mn, mx))
+    for h, p in hist.iteritems():
+        print("{:>2}: {:>6} [{:>8}]".format(h, p, (100.*p)/n_hands))
+
